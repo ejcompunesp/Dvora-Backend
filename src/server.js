c0bc14e59d1d1ceb161e1a6b7bdb7599c4578806
JohnnyBaptista <joaosbaptista@hotmail.com>
2019-11-21 12:49:39 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor NodeJS Rodando');
+});
