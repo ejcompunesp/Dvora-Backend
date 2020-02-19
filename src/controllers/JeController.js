5ae2e98c0f5fcf1191eea3738f0520af40902659
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-02-19 17:04:35 -0300

    iniciando parte de jwt

--- src/controllers/JeController.js
+++ src/controllers/JeController.js
@@ -1,5 +1,14 @@
 const Je = require('../models/Je');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+
+const generateHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+const validPassword = (password, hash) => bcrypt.compareSync(password, hash);
+
+const generateToken = (params = {}) => jwt.sign(params, authConfig.secret, {
+  expiresIn: 86400, //um dia
+});
 
 module.exports = {
   async index(req, res) {
@@ -19,34 +28,36 @@ module.exports = {
 
   async store(req, res) {
     let { name, email, password, university, image, creationYear } = req.body;
-    const generateHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 
     password = generateHash(password);
 
     try {
       const je = await Je.create({ name, email, password, university, image, creationYear });
-      return res.status(200).json(je);
-    } catch (je) {
-      return res.status(400).json({ msg: 'Erro nos dados inseridos' })
+      je.password = undefined;
+      return res.status(200).json({ je, token: generateToken({ id: je.id }) });
+    } catch (error) {
+      return res.status(400).json({ msg: 'ERROR' });
     }
   },
 
   async login(req, res) {
     let { email, password } = req.body;
-    const validPassword = (password, hash) => bcrypt.compareSync(password, hash);
     try {
-      const je = await Je.findOne({
+      let je = await Je.findOne({
         where: { email },
-      })
-      if (je.length == 0)
-        return res.status(200).json({ msg: 'EMAIL NOT FOUND' });
+      });
+      je = je.dataValues;
+      if (je == null)
+        return res.status(400).json({ msg: 'EMAIL NOT FOUND' });
       let ok = validPassword(password, je.password);
-      if (ok)
-        return res.status(200).json({ msg: 'OK' });
-      else
-        return res.status(200).json({ msg: 'INCORRECT PASSWORD' });
+      if (!ok)
+        return res.status(400).json({ msg: 'INCORRECT PASSWORD' });
+      else {
+        je.password = undefined;
+        return res.status(200).json({ je, token: generateToken({ id: je.id }) });
+      }
     } catch (error) {
-      return res.status(400).json({ msg: 'ERROR' });
+      return res.status(400).json(error);
     }
   },
 
