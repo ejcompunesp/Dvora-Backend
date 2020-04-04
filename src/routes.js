9af43a5c7c326db52f67a7da016b0b5ed4de700f
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-04-04 16:12:43 -0300

    arrumando para fazer dois tokens

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const JeController = require('./controllers/JeController');
 const MemberController = require('./controllers/MemberController');
 const LoginController = require('./controllers/LoginController');
-const authMiddleware = require('./middlewares/auth');
+const authJe = require('./middlewares/authJe');
+const authMember = require('./middlewares/authMember');
 
 const routes = express.Router();
 
@@ -14,14 +15,12 @@ routes.post('/login', LoginController.login);
 
 routes.get('/jes', JeController.index);
 routes.post('/jes/signup', JeController.store);
-// routes.post('/jes/login', JeController.login);
-routes.delete('/jes/delete', authMiddleware, JeController.delete);
-routes.put('/jes/update', authMiddleware, JeController.update);
+routes.delete('/jes/delete', authJe, JeController.delete);
+routes.put('/jes/update', authJe, JeController.update);
 
 routes.get('/jes/:jeId/members', MemberController.index);
 routes.post('/jes/:jeId/members/signup', MemberController.store);
-// routes.post('/members/login', MemberController.login);
-routes.delete('/jes/:jeId/members/delete', MemberController.delete);
-routes.put('/jes/:jeId/members/update', MemberController.update);
+routes.delete('/jes/:jeId/members/delete', authMember, MemberController.delete);
+routes.put('/jes/:jeId/members/update', authMember, MemberController.update);
 
 module.exports = routes;
