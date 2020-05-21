a9251d3ef61570d8e764b2891ca30339619b57a0
viniciusveu <vinicius_vedovotto@hotmail.com>
2020-05-21 12:10:19 -0300

    Alterações na tabela de membros e adição da coluna de tempo de plantão na tabela de EJs

---
+++ src/database/migrations/20200521144035-alter-table-members.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn('Members', 'facebook', {
+            type: Sequelize.STRING
+        }, { transaction: t }),
+        queryInterface.addColumn('Members', 'instagram', {
+            type: Sequelize.STRING,
+        }, { transaction: t }),
+        queryInterface.addColumn('Members', 'linkedin', {
+          type: Sequelize.STRING,
+        }, { transaction: t }),
+        queryInterface.removeColumn('Members', 'dutyDate', { transaction: t }),
+        queryInterface.removeColumn('Members', 'dutyTime', { transaction: t })
+      ])
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn('Members', 'facebook', { transaction: t }),
+        queryInterface.removeColumn('Members', 'instagram', { transaction: t }),
+        queryInterface.removeColumn('Members', 'linkedin', { transaction: t }),
+        queryInterface.addColumn('Members', 'dutyDate', {
+          type: Sequelize.STRING,
+        }, { transaction: t }),
+        queryInterface.addColumn('Members', 'dutyTime', {
+          type: Sequelize.STRING,
+        }, { transaction: t })
+      ])
+    })
+  }
+};
