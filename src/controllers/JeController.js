9090a23852648bea2ae03e4327856eb398180270
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-05-25 17:51:57 -0300

    arrumando mensagens de erro e o erro do email ja existir

--- src/controllers/JeController.js
+++ src/controllers/JeController.js
@@ -19,7 +19,7 @@ module.exports = {
     try {
       const jes = await Je.findAll();
       if (jes.length == 0)
-        return res.status(404).json({ error: 'JE NOT FOUND' });
+        return res.status(404).json({ msg: 'JE NOT FOUND' });
       else {
         for (let i = 0; i < jes.length; i++) {
           jes[i].password = undefined;
@@ -28,23 +28,32 @@ module.exports = {
         return res.status(200).json(jes);
       }
     } catch (error) {
-      return res.status(400).json({ error: 'ERROR WHEN GET JES' });
+      return res.status(400).json({ msg: 'ERROR WHEN GET JES' });
     }
   },
 
   async store(req, res) {
     const { name, email, password, university, image, city, creationYear } = req.body;
-    if (!name || name == null || name == undefined) errors.push({ error: 'NAME IS INVALID' })
-    if (!email || email == null || email == undefined) errors.push({ error: 'EMAIL IS INVALID' })
-    if (!password || password == null || password == undefined) errors.push({ error: 'PASSWORD IS INVALID' })
-    if (!university || university == null || university == undefined) errors.push({ error: 'UNIVERSITY IS INVALID' })
-    if (!city || city == null || city == undefined) errors.push({ error: 'CITY IS INVALID' })
-    if (!creationYear || creationYear == null || creationYear == undefined) errors.push({ error: 'CREATION YEAR IS INVALID' })
+    if (!name || name == null || name == undefined) errors.push({ msg: 'NAME IS INVALID' })
+    if (!email || email == null || email == undefined) errors.push({ msg: 'EMAIL IS INVALID' })
+    if (!password || password == null || password == undefined) errors.push({ msg: 'PASSWORD IS INVALID' })
+    if (!university || university == null || university == undefined) errors.push({ msg: 'UNIVERSITY IS INVALID' })
+    if (!city || city == null || city == undefined) errors.push({ msg: 'CITY IS INVALID' })
+    if (!creationYear || creationYear == null || creationYear == undefined) errors.push({ msg: 'CREATION YEAR IS INVALID' })
     if (errors.length > 0) return res.status(400).json(errors)
 
     hash = generateHash(password);
 
     try {
+      const query = await Je.findOne({ where: { email } });
+      if (query) {
+        if (req.file) {
+          const { key } = req.file;
+          promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'public', 'uploads', 'je', key));
+        }
+        return res.statsu(400).json({ msg: 'EMAIL ALREADY REGISTERED' });
+      }
+
       if (req.file) {
         const { key } = req.file;
         const je = await Je.create({ name, email, password: hash, university, image: key, city, creationYear });
@@ -61,14 +70,14 @@ module.exports = {
         const { key } = req.file;
         promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'public', 'uploads', 'je', key));
       }
-      return res.status(400).json(error);
+      return res.status(400).json({ msg: 'JE REGISTRATION ERROR' });
     }
   },
 
   async delete(req, res) {
     const { id } = req.body;
     if (!id || id == null || id == undefined)
-      return res.status(400).json({ error: 'JE ID IS INVALID' })
+      return res.status(400).json({ msg: 'JE ID IS INVALID' })
 
     try {
       const je = await Je.findByPk(id);
@@ -79,20 +88,20 @@ module.exports = {
         return res.status(200).json({ msg: 'JE DELETED SUCCESSFULLY' });
       }
       else
-        return res.status(400).json({ error: 'JE NOT FOUND' });
+        return res.status(400).json({ msg: 'JE NOT FOUND' });
     } catch (error) {
-      return res.status(400).json({ error: 'JE DELETE ERROR' });
+      return res.status(400).json({ msg: 'JE DELETE ERROR' });
     }
   },
 
   async update(req, res) {
     const { id, password, name, university, image, city, creationYear } = req.body;
-    if (!name || name == null || name == undefined) errors.push({ error: 'NAME IS INVALID' })
-    if (!email || email == null || email == undefined) errors.push({ error: 'EMAIL IS INVALID' })
-    if (!password || password == null || password == undefined) errors.push({ error: 'PASSWORD IS INVALID' })
-    if (!university || university == null || university == undefined) errors.push({ error: 'UNIVERSITY IS INVALID' })
-    if (!city || city == null || city == undefined) errors.push({ error: 'CITY IS INVALID' })
-    if (!creationYear || creationYear == null || creationYear == undefined) errors.push({ error: 'CREATION YEAR IS INVALID' })
+    if (!name || name == null || name == undefined) errors.push({ msg: 'NAME IS INVALID' })
+    if (!email || email == null || email == undefined) errors.push({ msg: 'EMAIL IS INVALID' })
+    if (!password || password == null || password == undefined) errors.push({ msg: 'PASSWORD IS INVALID' })
+    if (!university || university == null || university == undefined) errors.push({ msg: 'UNIVERSITY IS INVALID' })
+    if (!city || city == null || city == undefined) errors.push({ msg: 'CITY IS INVALID' })
+    if (!creationYear || creationYear == null || creationYear == undefined) errors.push({ msg: 'CREATION YEAR IS INVALID' })
     if (errors.length > 0) return res.status(400).json(errors)
 
     try {
@@ -133,7 +142,7 @@ module.exports = {
         const { key } = req.file;
         promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'public', 'uploads', 'je', key));
       }
-      return res.status(400).json(error);
+      return res.status(400).json({ msg: 'JE UPDATE ERROR' });
     }
   },
 };
