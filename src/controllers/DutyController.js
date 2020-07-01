1d510fdab512cd1c9b1d462788fa95c5704768b6
viniciusveu <vinicius_vedovotto@hotmail.com>
2020-07-01 14:09:53 -0300

    Alterações no controller de plantões

--- src/controllers/DutyController.js
+++ src/controllers/DutyController.js
@@ -7,6 +7,7 @@ const moment = MomentRange.extendMoment(Moment);
 
 const bcrypt = require('bcrypt');
 const { where } = require('sequelize');
+const sequelize = require('sequelize');
 const validPassword = (password, hash) => bcrypt.compareSync(password, hash);
 
 module.exports = {
@@ -47,7 +48,30 @@ module.exports = {
       if (!validPassword(password, member.password))
         return res.status(400).json({ error: 'INCORRECT PASSWORD' });
 
-      if (!member.isDutyDone) return res.status(409).json({ msg: 'ALREADY ON DUTY STARTED' })
+
+
+      // sequelize.query('SELECT MAX(?) FROM Duties;',
+      //   { replacements: ['createdAt'], type: sequelize.QueryTypes.SELECT }
+      // ).then(Duties => {
+      //   console.log(Duties)
+      //   return res.status(404).json({ error: 'PLANTÃO JA INICIADO'})
+      // })
+
+      // sequelize.query('SELECT * FROM Duties WHERE memberId = :member.id AND status = 0 ',
+      //   { replacements: { memberId: member.id }, type: sequelize.QueryTypes.SELECT }
+      // ).then(Duties => {
+      //   console.log(Duties)
+      //   return res.status(404).json({ error: 'PLANTÃO JA INICIADO'})
+      // })
+
+      const dutyIfExist = await Duty.findAll({
+        where: { memberId: member.id }
+      })
+      dutyIfExist.forEach( function(iten) {
+        if (iten.status == 0) 
+          return res.status(404).json({ error: 'PLANTÃO JA INICIADO'})
+      })
+
 
       const duty = await Duty.create({
         memberId: member.id,
@@ -55,6 +79,9 @@ module.exports = {
         elapsedTime: 0
       })
       member.password = undefined;
+      member.update({
+        isDutyDone: 0
+      })
       return res.status(201).json({ member, duty })
 
     } catch (error) {
@@ -82,6 +109,14 @@ module.exports = {
       duty.update({
         status: 1,
         elapsedTime
+      })
+
+      const member = await Member.findByPk(duty.memberId)
+      if (!member) 
+        return res.status(404).json({ msg: 'MEMBER NOT FOUND' })
+
+      member.update({
+        isDutyDone: 1
       })
 
       return res.status(200).json(duty);
